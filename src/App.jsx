bb65c475734942aa3a4ac77cb7647a2e0649bb09
marcelobicudo <marcelobicudo.silva@gmail.com>
2022-06-13 08:48:55 -0300

    corrigindo erro de path do arquivo e entendendo propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Marcelo Bicudo" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse soluta rerum! Ratione odit soluta nobis voluptatem quae fugiat similique vitae? Nisi illo suscipit eligendi recusandae dolorem et consequuntur iure." 
+      />
+      <Post 
+        author="João Silva" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse soluta rerum! Ratione odit soluta nobis voluptatem quae fugiat similique vitae? Nisi illo suscipit eligendi recusandae dolorem et consequuntur iure." 
+      />
     </div>
   )
 }
