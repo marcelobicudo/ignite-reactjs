cc07cba2b3df91ff34ffb5d8b655daa2944f9059
marcelobicudo <marcelobicudo.silva@gmail.com>
2022-06-13 13:57:40 -0300

    iniciando a codar a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Marcelo Bicudo" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse soluta rerum! Ratione odit soluta nobis voluptatem quae fugiat similique vitae? Nisi illo suscipit eligendi recusandae dolorem et consequuntur iure." 
-      />
-      <Post 
-        author="João Silva" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse soluta rerum! Ratione odit soluta nobis voluptatem quae fugiat similique vitae? Nisi illo suscipit eligendi recusandae dolorem et consequuntur iure." 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar /> 
+        <main>
+          <Post 
+            author="Marcelo Bicudo" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse soluta rerum! Ratione odit soluta nobis voluptatem quae fugiat similique vitae? Nisi illo suscipit eligendi recusandae dolorem et consequuntur iure." 
+          />
+          <Post 
+            author="João Silva" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse soluta rerum! Ratione odit soluta nobis voluptatem quae fugiat similique vitae? Nisi illo suscipit eligendi recusandae dolorem et consequuntur iure." 
+          />
+        </main>
+      </div>
     </div>
   )
 }
