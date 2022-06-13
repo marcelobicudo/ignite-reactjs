3326b5bd2180e6e5c8ce3e333486ec0eab331558
marcelobicudo <marcelobicudo.silva@gmail.com>
2022-06-13 14:22:39 -0300

    concluindo a sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,15 +11,17 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src='https://github.com/marcelobicudo.png' alt='Avatar'/>
+
                 <strong>Marcelo Bicudo</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href='#'>
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
-
             </footer>
         </aside>
     );
