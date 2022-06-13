77981b4a372f8e19bee1c7839a3ff1fb80a54f87
marcelobicudo <marcelobicudo.silva@gmail.com>
2022-06-13 09:16:27 -0300

    iniciando com css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="Marcelo Bicudo" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas esse soluta rerum! Ratione odit soluta nobis voluptatem quae fugiat similique vitae? Nisi illo suscipit eligendi recusandae dolorem et consequuntur iure." 
