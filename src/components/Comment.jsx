78c0945ca93fac223380dac3489e82dc4d79d825
marcelobicudo <marcelobicudo.silva@gmail.com>
2022-06-13 18:25:30 -0300

    criando componente de avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import styles from './Comment.module.css';
 import { Trash, ThumbsUp } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/marcelobicudo.png" alt="" />
+            <Avatar hasBorder={false} src="https://github.com/marcelobicudo.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
